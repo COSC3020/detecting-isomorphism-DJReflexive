44f2ef9f239f4f9eef1d7dddfb1de9f76909a8e4
Lexus Fermelia <lfermeli@uwyo.edu>
2024-10-24 11:49:36 -0600

    Not a submission. Autotest should fail

--- code_module/code.test.js
+++ code_module/code.test.js
@@ -61,7 +61,7 @@ function testSymmetric(size, numTests) {
     return true;
 }
 
-// Tests Permutations
+// Tests Valid Permutations
 function testPermutation(size, numTests) {
 
     // *Randomly* generates permutation to test
@@ -96,13 +96,45 @@ function testPermutation(size, numTests) {
     return true;
 }
 
+// Tests When Graphs are not Isomorphic
+function testInvalidGraphs(size, numTests) {
+    // Case where size is 0 passes test, even though it's already isomorphic
+    if (size == 0) {
+        return true;
+    }
+
+    for (let i = 0; i < numTests; i++) {
+        let originalGraph = generateGraph(size);
+
+        // Changes one edge so that the graphs are no longer isomorphic
+        let newGraph = originalGraph;
+        let newEdges = newGraph[0].getEdges();
+        
+        // Inverts the first edge, making graphs no longer isomorphic
+        if (newEdges[0] == 0) { newEdges[0] = 1; } 
+        else { newEdges[0] = 0; }
+
+        newGraph[0] = new GraphNode(newGraph[0].getNode(), newEdges);
+
+        console.log("DEBUG: origGraphEdges=" + originalGraph[0].getEdges())
+        console.log("     : newGraphEdges=" + newGraph[0].getEdges())
+        console.log("     : newEdges=" + newEdges)
+
+        if (are_isomorphic(originalGraph, newGraph)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 
 
 
     /* Custom Testing */
 
 const graphSize = 3; // 3: Not too large so that heap doesn't run out of memory
-const numOfTests = 1000; // 1000: Sufficiently large for n! runtime
+const numOfTests = 10; // 1000: Sufficiently large for n! runtime
 
 // Reflexive: graph is isomporphic to itself...
 const isReflexive = testReflexivity(graphSize, numOfTests);
@@ -110,11 +142,15 @@ const isReflexive = testReflexivity(graphSize, numOfTests);
 // Symmetry: graph1 is isomorphic to graph2 and vice versa...
 const isSymmetric = testSymmetric(graphSize, numOfTests);
 
-// Permutations: Permutations of the same graph are Isomorphic..
-const checkPermutation = testPermutation(graphSize, numOfTests)
+// Permutations: Permutations of the same graph are Isomorphic...
+const checkPermutation = testPermutation(graphSize, numOfTests);
 
-if (isReflexive && isSymmetric && checkPermutation) {
-    console.assert(isReflexive && isSymmetric && checkPermutation, 
+// InvalidGraph: Tests when graphs are not isomorphic...
+const checkInvalidGraphs = testInvalidGraphs(graphSize, numOfTests);
+
+if (isReflexive && isSymmetric && checkPermutation && checkInvalidGraphs) {
+    console.assert(isReflexive && isSymmetric && 
+                    checkPermutation && checkInvalidGraphs, 
                         "Tests have failed...");
 }
 else if (!isReflexive) {
@@ -125,6 +161,9 @@ else if (!isSymmetric) {
 }
 else if (!checkPermutation) {
     throw "A Permutation was found non-isomorphic..."
+}
+else if (!checkInvalidGraphs) {
+    throw "A graph was supposed to be found invalid..."
 }
 else {
     throw "Some unknown error!!!"
